b8b02f9ab6e07f389b7329364a51ea00bc21d3e3
Jorge Abad <jorgejoseabad@gmail.com>
2018-10-16 11:25:06 +0200

    added router and gallery login and signup comps

--- client-artcafe/src/App.js
+++ client-artcafe/src/App.js
@@ -1,26 +1,43 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+
+import {Gallery} from './components/galery.js';
+import {Login} from './components/login.js';
+import {Signup} from './components/signup.js';
+
+const Routes = () => {
+  return [
+    <Route exact path="/" key="r1" component={Gallery} />,
+    <Route path="/gallery" key="r1" component={Gallery}/>,
+    <Route path="/login" key="r2" component={Login} />,
+    <Route path="/signup" key="r3" component={Signup} />
+  ]
+  };
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <div
+              className="classRoutes"
+              style={{
+                backgroundColor:'hsla(33, 29%, 70%, 1)',
+                margin:20
+              }}>
+              <Routes />
+            </div>
+          </header>
+
+        </div>
+      </Router>
     );
   }
 }
