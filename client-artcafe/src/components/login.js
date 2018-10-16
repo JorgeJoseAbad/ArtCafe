b382537bfce1846f79dda600f196b6ef16d98489
Jorge Abad <jorgejoseabad@gmail.com>
2018-10-16 13:45:36 +0200

    added body-parser dotenv and test user routes

--- client-artcafe/src/components/login.js
+++ client-artcafe/src/components/login.js
@@ -10,10 +10,27 @@ export const Login = (props) =>{
     .catch(error=>console.log(error));
   }
 
+  function callBackEndPost(){
+    let prueba='prueba';
+    axios.post('http://localhost:3000/users',{prueba})
+    .then((res)=>console.log(res))
+    .catch(e=>console.log(e))
+  }
+
+  function callBackEndPostParam(){
+    let prueba='prueba';
+    axios.post('http://localhost:3000/users/jorge',{prueba})
+    .then((res)=>console.log(res))
+    .catch(e=>console.log(e))
+  }
+
+
    return(
         <div className="page">
           <pre>{props.match.path}</pre>
-          <button onClick={callBackEnd}>test call</button>
+          <button onClick={callBackEnd}>test get</button>
+          <button onClick={callBackEndPost}>test post</button>
+          <button onClick={callBackEndPostParam}>test post</button>
           <img alt="CarlosSainz" src="https://instagram.fmad3-6.fna.fbcdn.net/vp/e2831892fe54726bc725854514d818bb/5C4D2598/t51.2885-15/sh0.08/e35/s640x640/43438078_333374877223205_9014798235918047786_n.jpg"/>
         </div>
    )
--- server-artcafe/routes/users.js
+++ server-artcafe/routes/users.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* POST users listing. this is a test OK*/
+router.post('/',((req,res,next)=>{
+    res.send(req.body);
+  })
+)
+
+/* POST users listing with params. this is a test OK*/
+router.post('/:id',((req,res,next)=>{
+    console.log(req.params);
+    res.send(req.body);
+  })
+)
+
 module.exports = router;
