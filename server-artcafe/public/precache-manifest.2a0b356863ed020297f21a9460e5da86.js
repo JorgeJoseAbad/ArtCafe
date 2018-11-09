1fca2ff85edfbde5c4b470ef751b0c317fbddd28
Jorge Abad <jorgejoseabad@gmail.com>
2018-11-09 19:13:54 +0100

    cliente build for prod, in public server

---
+++ server-artcafe/public/precache-manifest.2a0b356863ed020297f21a9460e5da86.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c37c0305e4a5ae8c3cbd48fa6285a73",
+    "url": "/static/media/fondo.3c37c030.jpg"
+  },
+  {
+    "revision": "d40f53bf44aa5e617437e0dadba1ad07",
+    "url": "/static/media/ArtCoffee-text.d40f53bf.png"
+  },
+  {
+    "revision": "f7930d87661cdaac7992a18d7d8d6a1f",
+    "url": "/static/media/ArtCoffee-logo.f7930d87.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7f743486f09f709f5e52",
+    "url": "/static/js/main.7f743486.chunk.js"
+  },
+  {
+    "revision": "6ed895a6c4f7465a61d5",
+    "url": "/static/js/1.6ed895a6.chunk.js"
+  },
+  {
+    "revision": "7f743486f09f709f5e52",
+    "url": "/static/css/main.0572fe45.chunk.css"
+  },
+  {
+    "revision": "5f415c5a964c420366d171aba49fa1e1",
+    "url": "/index.html"
+  }
+];
