58f3a4a0c53657bf90df62ee0c5ee27927ad0635
Jorge Abad <jorgejoseabad@gmail.com>
2018-11-13 20:39:15 +0100

    modif in multer conf

--- server-artcafe/routes/gallery.js
+++ server-artcafe/routes/gallery.js
@@ -5,7 +5,7 @@ const Artwork = require("../models/artwork.js");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public/uploads/')
+    cb(null, '/public/uploads')
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + '-' + Date.now()+'.jpg')
--- server-artcafe/routes/users.js
+++ server-artcafe/routes/users.js
@@ -5,7 +5,7 @@ const User = require("../models/user");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public/uploads/')
+    cb(null, '/public/uploads')
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + '-' + Date.now()+'.jpg')
